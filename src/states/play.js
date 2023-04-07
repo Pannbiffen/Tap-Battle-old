8de5a1be7de6ead5c073c34973113c7a05c2d672
Pannbiff <martin.mupp@gmail.com>
2023-04-07 21:47:29 +0200

    test utan build

--- src/states/play.js
+++ src/states/play.js
@@ -2,7 +2,7 @@ import Phaser from 'phaser'
 
 class Play extends Phaser.State {
   create () {
-    this.game.stage.backgroundColor = '#000'
+    this.game.stage.backgroundColor = '#4a0500'
   }
 
   update () {
